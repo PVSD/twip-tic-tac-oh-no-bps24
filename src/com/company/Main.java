16ad2fcd39ad98f81a6bc7b8617e0957fc27847a
bs482 <bs482@pvsd.org>
2018-12-20 11:19:42 -0500

    almost done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,27 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
+
+
         System.out.println("What version would you like to play\n" +
                 "Enter 1: Player vs Player\n" +
                 "Enter 2: Player vs Computer\n" +
-                "Enter 3: Computer vs Computer");
+                "Enter 3: Computer vs Player\n" +
+                "Enter 4: Computer vs Computer");
         int version = kb.nextInt();
 
         char[] b={' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        char a1='X';
+        char a2='O';
+
+        if(version==1) board.game1(b,a1,a2);
 
 
-        board.firstboard();
-
-        while(true)
-        {
-            System.out.println("Player 1 enter a number");
-            if(b[kb.nextInt()]==' ');
-            {
-                
-            }
-            board.makeBoard(b);
-
-        }
 
 
 
--- src/com/company/board.java
+++ src/com/company/board.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 /**
  * Created by bs482 on 12/17/18.
  */
@@ -7,6 +9,8 @@ public class board {
 
     public board()
     {}
+
+    public static int a;
 
     public static void makeBoard(char b[])
     {
@@ -23,7 +27,7 @@ public class board {
             }
             if(j!=2) a+="\n-----------\n";
         }
-        System.out.print(a);
+        System.out.print(a+"\n");
     }
 
     public static void firstboard()
@@ -31,5 +35,80 @@ public class board {
         System.out.print(" 0 | 1 | 2 \n-----------\n 3 | 4 | 5 \n-----------\n 6 | 7 | 8 \n\n");
     }
 
+    public static void playerMove(char [] c, char z)
+    {
+        Scanner kb = new Scanner(System.in);
+
+        while(true)
+        {
+            System.out.println("\nPlayer 1 enter a number");
+            a=kb.nextInt();
+
+            if(c[a]==' ')
+            {
+                c[a]=z;
+                board.makeBoard(c);
+                break;
+            }
+            else System.out.println("Enter an open region.");
+        }
+    }
+
+    public static boolean check (char [] c, char z)
+    {
+        if (c[0] == z && c[3] == z && c[6] == z
+                ||c[1] == z && c[4] == z && c[7] == z
+                ||c[2] == z && c[5] == z && c[8] == z
+                ||c[0] == z && c[1] == z && c[2] == z
+                ||c[3] == z && c[4] == z && c[5] == z
+                ||c[6] == z && c[7] == z && c[8] == z
+                ||c[0] == z && c[4] == z && c[8] == z
+                ||c[2] == z && c[4] == z && c[6] == z)
+            return true;
+        else return false;
+    }
+
+    public static void game1(char [] c, char z1, char z2)
+    {
+        board.firstboard();
+        for(int i=0; i<5;i++)
+        {
+            board.playerMove(c, z1);
+            if(board.check(c, z1))
+            {
+                System.out.print("\nPlayer 1 wins");
+                break;
+            }
+
+            board.playerMove(c, z2);
+            if(board.check(c, z2))
+            {
+                System.out.println("\nPlayer 2 wins");
+                break;
+            }
+        }
+    }
+
+    public static void game2(char [] c, char z1, char z2)
+    {
+        board.firstboard();
+        for(int i=0; i<5;i++)
+        {
+            board.playerMove(c, z1);
+            if (board.check(c, z1))
+            {
+
+                System.out.print("\nPlayer 1 wins");
+                break;
+            }
+            board.compmove2(c,z1,z2);
+        }
+
+    }
+
+    public static void compmove2(char[]c, char z1, char z2)
+    {
+        if(c[])
+    }
 
 }
